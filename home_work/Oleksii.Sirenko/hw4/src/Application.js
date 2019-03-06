b44afe8682feed359d9813531fb6e3c2da672611
Sirenko, Oleksii <Oleksii.Sirenko@dish.com>
2019-03-06 11:47:37 +0200

    [HW4] Create two React components

---
+++ home_work/Oleksii.Sirenko/hw4/src/Application.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import Month from "./components/month-component";
+
+export default class Application extends Component {
+    render() {
+        return <Month />;
+    }
+}
