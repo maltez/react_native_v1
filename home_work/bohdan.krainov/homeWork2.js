92bc4ac388d63c1e9a65bcc74dd38d06c42694b1
Bohdan.Krainov <Bohdan.Krainov@dish.com>
2019-03-19 09:11:04 -0600

    test hw2

--- home_work/bohdan.krainov/homeWork2.js
+++ home_work/bohdan.krainov/homeWork2.js
@@ -4,8 +4,13 @@ randWaitTime = () => {
 
 function fakeAPI (msg, delay){
     return new Promise((resolve, reject)=>{
-        if(delay > 7) {
+        if(delay > 700) {
+            console.log('???')
             return new RangeError('Its too long')
+        }
+        if(msg.includes('error')) {
+            console.log('!!!!');
+            return new RangeError("Parameter contains error")
         }
         setTimeout(()=> {
             return resolve(msg);
@@ -13,6 +18,20 @@ function fakeAPI (msg, delay){
     });
 }
 
+promises = [];
+
 for(let i=0; i<7; i++) {
-    console.log(randWaitTime());
+    let time = randWaitTime();
+    console.log(time);
+    promises.push(fakeAPI('Hello', time));
 }
+
+console.log(promises);
+
+Promise.all(promises)
+    .then(dataArray => {
+        console.log("Result of Promise.all", dataArray);
+        return dataArray;
+    })
+    .then(data => {
+        console.log("Result after Promise.all", data);})
