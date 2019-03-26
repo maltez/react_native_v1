9ff0b5482b2c1971b9a92774660d416240e8b796
Bohdan.Krainov <Bohdan.Krainov@dish.com>
2019-03-26 08:24:38 -0600

    new update;

--- home_work/bohdan.krainov/homeWork2.js
+++ home_work/bohdan.krainov/homeWork2.js
@@ -2,10 +2,14 @@ randWaitTime = () => {
     return Math.floor(Math.random()*1000);
 }
 
+test_url_1 = 'http://testurl1.com';
+test_url_2 = 'http://testurl2.com';
+test_url_3 = 'http://testurl3.com'
+
 function fakeAPI (msg, delay){
     return new Promise((resolve, reject)=>{
         if(msg.includes('error')) {
-            return reject(new Error("Parameter contains error"))
+            return reject(new Error(`Parameter "${msg}" contains error`))
         }
         setTimeout(()=> {
             return resolve({'message':msg, 'time': delay});
@@ -13,12 +17,19 @@ function fakeAPI (msg, delay){
     });
 }
 
+function updateURL(url, key, values) {
+    result = url + '/?'
+    for (i of values) {
+        result += `${key}=${i}`
+    }
+    return result
+}
+
 fakeAPI('first', 300)
     .then(result => {
-        console.log(result, '???');
+        console.log(result);
         return fakeAPI("with error", 500);
     })
-    .then(result => console.log(result))
     .catch((error) => {
         console.log(error);
     })
@@ -28,16 +39,20 @@ promises = [];
 
 for(let i=0; i<7; i++) {
     let time = randWaitTime();
-    promises.push(fakeAPI(i.toString(), time));
+    promises.push(fakeAPI(`param${i}`, time));
 }
+
+final_requests = [];
 
 Promise.all(promises)
     .then(dataArray => {
-        console.log("Result of Promise.all");
+        console.log("Result of all Promises");
         console.log(dataArray);
+        for(i of dataArray) {
+            console.log(i.message);
+        }
         return dataArray;
     })
     .catch((err) => {
-        console.log('@@@');
         console.log(err.message);
     });
