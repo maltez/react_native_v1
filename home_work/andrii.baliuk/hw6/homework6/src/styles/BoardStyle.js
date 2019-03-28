9d6a3b71781ebb3e964d320f1b24c4bff2c6193b
Andrii Baliuk <zerksis.break@gmail.com>
2019-03-28 22:16:07 +0200

    homework #6 - Done

---
+++ home_work/andrii.baliuk/hw6/homework6/src/styles/BoardStyle.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  boardConatiner: {
+    borderWidth: 1,
+    borderColor: "#000000"
+  },
+  boardRow: {
+    flexDirection: "row"
+  }
+});
+
+export default styles;
